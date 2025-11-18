63f39c6e7c0e892080df9d61ca8d2717e5c47835
CHAD_mulligan <itskenedy5@gmail.com>
2025-11-18 18:28:28 +0300

    test live

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Gaming from './game/Gaming';
+import Gaming from './gaming/Gaming';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
